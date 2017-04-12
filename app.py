b37478d9d5a962ce77a1c6b1cd4d7e8442c6370d
flowsnow <snowlight437@gmail.com>
2017-04-12 16:17:50 +0800

    [0x10] 使用装饰器动态注册路由

--- app.py
+++ app.py
@@ -12,25 +12,15 @@ from webob.dec import wsgify
 from webob import exc
 
 
-def hello(request: Request) -> Response:
-    name = request.params.get("name", 'default_name')
-    response = Response()
-    response.text = 'hello {}'.format(name)
-    response.status_code = 200
-    response.content_type = 'text/plain'
-    return response
-
-
-def root(request: Request) -> Response:
-    return Response(body='hello world', status=200, content_type='text/plain')
-
-
 class Application:
     router = {}
 
     @classmethod
-    def register(cls, path, handler):
-        cls.router[path] = handler
+    def register(cls, path):
+        def wrap(handler):
+            cls.router[path] = handler
+            return handler
+        return wrap
 
     @wsgify
     def __call__(self, request: Request) -> Response:
@@ -40,11 +30,23 @@ class Application:
             return exc.HTTPNotFound('{} not found'.format(request.path))
 
 
+@Application.register('/hello')
+def hello(request: Request) -> Response:
+    name = request.params.get("name", 'default_name')
+    response = Response()
+    response.text = 'hello {}'.format(name)
+    response.status_code = 200
+    response.content_type = 'text/plain'
+    return response
+
+
+@Application.register('/')
+def root(request: Request) -> Response:
+    return Response(body='hello world', status=200, content_type='text/plain')
+
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
 
-    Application.register('/hello', hello)
-    Application.register('/', root)
     server = make_server('0.0.0.0', 9000, Application())
     try:
         print('Serving on port 9000...')
